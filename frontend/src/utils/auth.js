086fd6166d00de008d6a1c25ffbdebfb00d1e7e0
mariawright05 <mariawright05@users.noreply.github.com>
2021-03-04 02:07:57 -0600

    Change base URL

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = 'https://register.nomoreparties.co';
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'http://api.mmw.students.nomoreparties.site';
 
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
